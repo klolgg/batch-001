45559f90c75d4fdd00c8fb1cf9aaec1b2cd2797c
seung-woo-ryu <37530565+seung-woo-ryu@users.noreply.github.com>
2024-12-16 17:35:02 +0900

    Initial commit

---
+++ src/main/java/site/klol/batch001/Batch001Application.java
@@ -0,0 +1,13 @@
+package site.klol.batch001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch001Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/klol/batch001/Batch001ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.klol.batch001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
