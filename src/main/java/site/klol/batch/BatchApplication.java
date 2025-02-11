eb7e20f85f68819f5a3a83cee8f3a704a6e94d3a
seung-woo-ryu <tmddn645@naver.com>
2025-02-11 15:55:43 +0900

    [feature/13] urlResolver, riot.yml변경

--- src/main/java/site/klol/batch/BatchApplication.java
+++ src/main/java/site/klol/batch/BatchApplication.java
@@ -2,9 +2,11 @@ package site.klol.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "site.klol.batch.riot.config")
 @EnableScheduling
 public class BatchApplication {
 
